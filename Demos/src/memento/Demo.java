ac7431639bca3bd25b92e6ff442d550de6d28985
KyleRobison15 <kylerobison15@gmail.com>
2025-12-27 04:00:50 -0700

    Memento demo

---
+++ Demos/src/memento/Demo.java
@@ -0,0 +1,30 @@
+package memento;
+
+public class Demo {
+    public static void show(){
+        var editor = new Editor();
+        var history = new History();
+
+        history.push(editor.createState());
+        System.out.println(editor.getContent()); // null
+
+        editor.setContent("A");
+        System.out.println(editor.getContent()); // A
+        history.push(editor.createState());
+
+        editor.setContent("B");
+        System.out.println(editor.getContent()); // B
+        history.push(editor.createState());
+
+        editor.setContent("D");
+        System.out.println(editor.getContent()); // D
+
+        editor.restore(history.pop()); // Undo
+        System.out.println(editor.getContent()); // B
+
+        editor.setContent("C");
+        System.out.println(editor.getContent()); // C
+        history.push(editor.createState());
+
+    }
+}
